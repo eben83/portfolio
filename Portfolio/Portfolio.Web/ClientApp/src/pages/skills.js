73d628de6a366f9a7c3067dbde02b479b736be3f
Eben Burger <eben@arlowconsulting.co.za>
2022-05-02 00:30:39 +0200

    test skill added to skills

--- Portfolio/Portfolio.Web/ClientApp/src/pages/skills.js
+++ Portfolio/Portfolio.Web/ClientApp/src/pages/skills.js
@@ -1,9 +1,11 @@
 import React, {} from 'react';
+import Skill from "../components/skill";
 
 const Skills = () => {
   return (
     <>
       <h4>Skills</h4>
+      <Skill title={"test"} text={'text'} />
     </>
   );
 }
