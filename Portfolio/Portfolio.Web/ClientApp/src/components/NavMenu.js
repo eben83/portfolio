ac2f3fc13613bcce3ad767b9eb422917ab845359
Eben Burger <eben@arlowconsulting.co.za>
2022-05-01 11:31:36 +0200

    updated nav to functional component

--- Portfolio/Portfolio.Web/ClientApp/src/components/NavMenu.js
+++ Portfolio/Portfolio.Web/ClientApp/src/components/NavMenu.js
@@ -1,49 +1,49 @@
-import React, { Component } from 'react';
+import React, {Component, useState} from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+import styled from "styled-components";
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
-
-  constructor (props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
+const NavMenu = (props) => {
+  
+  const [collapse, setCollapse] = useState(false)
+    
+  
+  const toggleNavbar = () => {
+    setCollapse(!collapse)
   }
+  
+  
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render () {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">Portfolio.Web</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
+        <NavWrapper>
+          <Navbar className="navbar-expand-sm navbar-toggleable-sm ">
+            <Container>
+              <NavbarBrand className='text-white' tag={Link} to="/">Eben Buger</NavbarBrand>
+              <NavbarToggler onClick={toggleNavbar} className="mr-2 text-white" />
+              <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={collapse} navbar>
+                <ul className="navbar-nav flex-grow">
+                  <NavItem>
+                    <NavLink tag={Link} to="/">Home</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink tag={Link} to="/">Projects</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink tag={Link} to="/">Contact</NavLink>
+                  </NavItem>
+                </ul>
+              </Collapse>
+            </Container>
+          </Navbar>
+        </NavWrapper>
       </header>
     );
-  }
 }
+
+const NavWrapper = styled.div`
+  background: #252734;
+`
+
+export default NavMenu
