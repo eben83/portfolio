2fc8d547190e36f930464b03a33aef59787939e7
Eben Burger <eben@arlowconsulting.co.za>
2022-05-01 12:27:10 +0200

    home page image and welcome text added

--- Portfolio/Portfolio.Web/ClientApp/src/components/home.js
+++ Portfolio/Portfolio.Web/ClientApp/src/components/home.js
@@ -1,11 +1,24 @@
 import React, {} from 'react';
 import styled from 'styled-components'
+import Me from '../images/me.jpg'
 
 const Home = () => {
     return (
         <>
             <Wrapper>
-                <Title>Coming Soon!</Title>
+              <LeftSide>
+                <Heading>
+                  <Hello>Hello! I'm</Hello>
+                  <Name>Eben Burger</Name>
+                  <p className='text-white-50'>Self Taught Frontend Developer</p>
+                </Heading>
+              </LeftSide>
+              <RightSide>
+                <img src={Me} alt='Image of me' />
+                <RoundShape>
+                  
+                </RoundShape>
+              </RightSide>
             </Wrapper>
         </>
     );
@@ -13,16 +26,68 @@ const Home = () => {
 
 const Wrapper = styled.div `
   position: relative;
-  background: #000000;
-  height: 100vh;
+  background: #252734;
+  height: 60vh;
+  color: white;
+  display: flex;
 `
 
-const Title = styled.h4 `
+const LeftSide = styled.div`
+  width: 50%;
+  z-index: 20;
+`
+const Heading = styled.div`
   position: absolute;
   top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: #ffffff;
+  left: 25%;
+  transform: translate(-50%, 0);
+  z-index: 10;
+`
+const Hello = styled.h2`
+  font-size: 5rem;
+`
+const Name = styled.h1`
+  font-size: 8rem;
+`
+
+const RightSide = styled.div`
+  position: relative;
+  height: 55rem;
+  width: 100%;
+  overflow: hidden;
+  background-color: red;
+  
+  img {
+    position: absolute;
+    top: -3rem;
+    right: 0;
+    z-index: 0;
+  }
+`
+
+const RoundShape = styled.div`
+  width: 400px;
+  height: 800px;
+  border-top-right-radius: 400px;
+  border-bottom-right-radius: 400px;
+  
+  &:before {
+    bottom: 68px;
+    left: -75px;
+    width: 400px;
+    height: 660px;
+    border-top-right-radius: 400px;
+    border-bottom-right-radius: 400px;
+  }
+  
+  &:after {
+    bottom: -46px;
+    left: 24px;
+    width: 450px;
+    height: 900px;
+    border-top-right-radius: 450px;
+    border-bottom-right-radius: 450px;
+  }
 `
 
 
