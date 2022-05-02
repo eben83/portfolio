5e1cd602da77a62931ca1d5a987d43994034fd20
Eben Burger <eben@arlowconsulting.co.za>
2022-05-02 19:43:26 +0200

    information, proj and Projects Scss and UI added

---
+++ Portfolio/Portfolio.Web/ClientApp/src/components/project/project.js
@@ -0,0 +1,13 @@
+import React, {} from 'react';
+
+
+import './project.xs.css'
+
+const Project = (props) => {
+  return (
+    <>
+      <div className='project'></div>
+    </>
+  );
+}
+export default Project;
