b6095e70b91dff60c18f1300752e81e966feb914
Eben Burger <eben@arlowconsulting.co.za>
2022-05-02 19:46:34 +0200

    skills updated

--- Portfolio/Portfolio.Web/ClientApp/src/pages/skills/skills.js
+++ Portfolio/Portfolio.Web/ClientApp/src/pages/skills/skills.js
@@ -14,22 +14,27 @@ const Skills = () => {
         <div className='title'>
           <h4>Skills</h4>
         </div>
-        <div className='skillItem'>
-          <Skill title={"HTML"} text={'text'} />
-          <Skill title={"CSS"} text={'text'} />
-          <Skill title={"ScSS"} text={'text'} />
-          <Skill title={"React Js"} text={'text'} />
-          <Skill title={"Vue"} text={'text'} />
-          <Skill title={"Flexbox"} text={'text'} />
-          <Skill title={"FireBase"} text={'text'} />
-          <Skill title={"VueX"} text={'text'} />
-          <Skill title={"Web Development"} text={'text'} />
-          <Skill title={"Frontend Develop"} text={'text'} />
-          <Skill title={"Bootstrap"} text={'text'} />
-          <Skill title={"BootstrapVue"} text={'text'} />
-          <Skill title={"Reactstrap"} text={'text'} />
+        <div className='skillsContainer'>
+          <div className='skillItem'>
+            <Skill title={"HTML"} text={'text'} />
+            <Skill title={"CSS"} text={'text'} />
+            <Skill title={"ScSS"} text={'text'} />
+            <Skill title={"React Js"} text={'text'} />
+            <Skill title={"Vue"} text={'text'} />
+            <Skill title={"Flexbox"} text={'text'} />
+            <Skill title={"FireBase"} text={'text'} />
+            <Skill title={"VueX"} text={'text'} />
+            <Skill title={"Web Development"} text={'text'} />
+            <Skill title={"Frontend Develop"} text={'text'} />
+            <Skill title={"Bootstrap"} text={'text'} />
+            <Skill title={"BootstrapVue"} text={'text'} />
+            <Skill title={"Reactstrap"} text={'text'} />
+            <Skill title={"Styled Components"} text={'text'} />
+            <Skill title={"Git"} text={'text'} />
+            <Skill title={"Googling"} text={'text'} />
+          </div>
         </div>
-      </div>
+        </div>
     </>
   );
 }
