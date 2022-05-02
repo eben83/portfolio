ba9f8b058c5941cec620022f785e0e42dc0240e6
Eben Burger <eben@arlowconsulting.co.za>
2022-05-02 17:48:42 +0200

    information UI added

--- Portfolio/Portfolio.Web/ClientApp/src/pages/Information/information.js
+++ Portfolio/Portfolio.Web/ClientApp/src/pages/Information/information.js
@@ -1,17 +1,11 @@
 import React, {} from 'react';
-import FunInformation from "../../components/funInfomation/funInformation";
-import {faGit} from "@fortawesome/free-brands-svg-icons";
+import FunInfo from "../../components/funInfo/funInfo";
 
 const Information = (props) => {
   return (
     <>
       <div className='information'>
-        <FunInformation 
-          icon={faGit}
-          numberFrom={0}
-          numberTo={10}
-          text='hello'
-        />
+        <FunInfo />
       </div>
     </>
   );
