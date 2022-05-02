ff5e71657398c2299499e04a4c99a1b60db438cc
Eben Burger <eben@arlowconsulting.co.za>
2022-05-02 19:46:23 +0200

    information UI funInfo Icons added

--- Portfolio/Portfolio.Web/ClientApp/src/pages/Information/information.js
+++ Portfolio/Portfolio.Web/ClientApp/src/pages/Information/information.js
@@ -1,11 +1,38 @@
 import React, {} from 'react';
 import FunInfo from "../../components/funInfo/funInfo";
+import {faCoffee, faDiagramProject, faHourglass} from "@fortawesome/free-solid-svg-icons";
+
+import './information.xs.css'
 
 const Information = (props) => {
   return (
     <>
       <div className='information'>
-        <FunInfo />
+        <div className='title'>
+          <h3>Fun Facts</h3>
+        </div>
+        <div className='infoItem'>
+          <FunInfo
+          title='Cups Of Coffee'  
+          number='2000'
+          icon={faCoffee}
+          />
+          <FunInfo
+            title='Hours Spent'
+            number='7500+'
+            icon={faHourglass}
+          />
+          <FunInfo
+            title='Private Repos'
+            number='21'
+            icon={faDiagramProject}
+          />
+          <FunInfo
+            title='Work Repos'
+            number='15'
+            icon={faDiagramProject}
+          />
+        </div>
       </div>
     </>
   );
