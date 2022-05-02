74310b97deb3af5a706a37d3265b0e86f9e836e9
Eben Burger <eben@arlowconsulting.co.za>
2022-05-02 19:45:38 +0200

    fun info comp updated

--- Portfolio/Portfolio.Web/ClientApp/src/components/funInfo/funInfo.js
+++ Portfolio/Portfolio.Web/ClientApp/src/components/funInfo/funInfo.js
@@ -2,17 +2,19 @@ import React, {} from 'react';
 
 import './funInfo.xs.css'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCoffee} from "@fortawesome/free-solid-svg-icons";
 
-const FunInfo = (props) => {
+
+const FunInfo = ({title, number, icon}) => {
   return (
     <>
       <div className='funInfo'>
         <div className='funItem'>
-          <FontAwesomeIcon icon={faCoffee} />
+          <div className='icon'>
+            <FontAwesomeIcon icon={icon} />
+          </div>
           <div className='funItemText'>
-            <h6>Cups of Coffe</h6>
-            <p>2000</p>
+            <h6>{title}</h6>
+            <p>{number}</p>
           </div>
         </div>
       </div>
