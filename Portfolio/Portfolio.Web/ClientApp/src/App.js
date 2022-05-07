c1611c7dc575caa391f453ce94299a407214c987
Eben Burger <eben@arlowconsulting.co.za>
2022-05-07 22:23:04 +0200

    updated app removed media queries

--- Portfolio/Portfolio.Web/ClientApp/src/App.js
+++ Portfolio/Portfolio.Web/ClientApp/src/App.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useRef, useState} from 'react';
-import { Route } from 'react-router-dom';
-import { useMediaQuery } from "react-responsive";
+import {
+  ScrollingProvider,
+  Section,
+} from 'react-scroll-section';
 
 import Layout from './components/layout/Layout';
 import Home from './pages/home/home';
@@ -37,31 +39,25 @@ const App = () => {
       behavior: 'smooth',
     });
   };
-
-  const isXxxl = useMediaQuery({query:'(min-width: 1680px)'})
-  const isXxl = useMediaQuery({query:'(min-width: 1440px)'})
-  const isXl = useMediaQuery({query:'(min-width: 1200px)'})
-  const isLg = useMediaQuery({query:'(min-width: 992px)'})
-  const isMd = useMediaQuery({query:'(min-width: 768px)'})
-  const isSm = useMediaQuery({query:'(min-width: 576px)'})
-
-  const media =
-      isXxxl ? "xxxl" :
-          isXxl ? "xxl" :
-              isXl ? "xl" :
-                  isLg ? "lg" :
-                      isMd ? "md" :
-                          isSm ? "sm" : "xs"
-
-
+  
     return (
       <>
-        <Layout media={media}>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/' component={Skills} />
-          <Route exact path='/' component={Information} />
-          <Route exact path='/' component={Experience} />
-        </Layout>
+        <ScrollingProvider>
+          <Layout>
+            <Section  id='home'>
+              <Home />
+            </Section>
+            <Section  id='skills'>
+              <Skills />
+            </Section>
+            <Section  id='information'>
+              <Information />
+            </Section>
+            <Section  id='experience'>
+              <Experience />
+            </Section>
+          </Layout>
+        </ScrollingProvider>
 
         <div className='scrollTop'
              onClick={scrollTop}
