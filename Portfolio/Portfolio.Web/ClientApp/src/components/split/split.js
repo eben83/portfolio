86901ade63575a276e081ab79909f89c2f277e9e
Eben Burger <eben@arlowconsulting.co.za>
2022-05-07 23:16:35 +0200

    split UI added to assist the space of the component for the scroll nav

---
+++ Portfolio/Portfolio.Web/ClientApp/src/components/split/split.js
@@ -0,0 +1,12 @@
+import React, {} from 'react';
+
+import './split.xs.css'
+
+const Split = (props) => {
+  return (
+    <>
+      <div className='split' />
+    </>
+  );
+}
+export default Split;
