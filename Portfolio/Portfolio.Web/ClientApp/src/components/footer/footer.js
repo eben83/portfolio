0692458a88f69f9934913b8e749209109d2035f4
Eben Burger <eben@arlowconsulting.co.za>
2022-05-08 11:46:01 +0200

    footer UI updated and styling tweaks

--- Portfolio/Portfolio.Web/ClientApp/src/components/footer/footer.js
+++ Portfolio/Portfolio.Web/ClientApp/src/components/footer/footer.js
@@ -2,6 +2,7 @@
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEnvelope, faMobile} from "@fortawesome/free-solid-svg-icons";
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
 
 import './footer.xs.css'
 import './footer.sm.css'
@@ -13,18 +14,29 @@ const Footer = (props) => {
   return (
     <>
       <div className='footer'>
-        <p>
+        <div className='contact'>
+          <p>
           <span>
             <FontAwesomeIcon className='icon' icon={faEnvelope} />
           </span>
-          eben_burger@me.com
-        </p>
-        <p>
+            eben_burger@me.com
+          </p>
+          <p>
           <span>
             <FontAwesomeIcon className='icon' icon={faMobile} />
           </span>
-          +27 (0) 73 730 0189
-        </p>
+            +27 (0) 73 730 0189
+          </p>
+        </div>
+        <div className='social'>
+          <a href='https://linkedin.com/in/eben-burger-856453145'>
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <p>Eben Burger - 2022</p>
+          <a href='https://github.com/eben83' >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
       </div>
     </>
   );
