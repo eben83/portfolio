e999dc3b40cec2f12fcce6251b96cef40af18447
Eben Burger <eben@arlowconsulting.co.za>
2022-05-08 11:14:54 +0200

    added about and all missing styling sheets

---
+++ Portfolio/Portfolio.Web/ClientApp/src/components/footer/footer.js
@@ -0,0 +1,33 @@
+﻿import React, {} from 'react';
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope, faMobile} from "@fortawesome/free-solid-svg-icons";
+
+import './footer.xs.css'
+import './footer.sm.css'
+import './footer.md.css'
+import './footer.lg.css'
+import './footer.xl.css'
+
+const Footer = (props) => {
+  return (
+    <>
+      <div className='footer'>
+        <p>
+          <span>
+            <FontAwesomeIcon className='icon' icon={faEnvelope} />
+          </span>
+          eben_burger@me.com
+        </p>
+        <p>
+          <span>
+            <FontAwesomeIcon className='icon' icon={faMobile} />
+          </span>
+          +27 (0) 73 730 0189
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
---
+++ Portfolio/Portfolio.Web/ClientApp/src/pages/about/about.js
@@ -0,0 +1,16 @@
+import React, {} from 'react';
+
+import './about.xs.css'
+import './about.sm.css'
+import './about.md.css'
+import './about.lg.css'
+import './about.xl.css'
+
+const About = (props) => {
+  return (
+    <>
+      <h4 id='about'> About</h4>
+    </>
+  );
+}
+export default About;
